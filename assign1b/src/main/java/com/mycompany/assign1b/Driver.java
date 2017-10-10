2b20f24fa02a5409b085b42676ee657f56bd98c9
Chris Cornwall <c.cornwall1@nuigalway.ie>
2017-10-10 12:43:19 +0100

    Initial Commit

---
+++ assign1b/src/main/java/com/mycompany/assign1b/Driver.java
@@ -0,0 +1,15 @@
+package com.mycompany.assign1b;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author cristoir95
+ */
+public class Driver {
+    
+}
